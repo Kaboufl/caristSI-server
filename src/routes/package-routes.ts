34011e76b5574fcf5e812aac210accd307ef9a42
Kaboufl <mw3pumat8@gmail.com>
2024-03-23 16:57:39 +0100

    catch duplicate entry et supression colis

--- src/routes/package-routes.ts
+++ src/routes/package-routes.ts
@@ -41,10 +41,54 @@ router.post("/package", expressAsyncHandler(
       })
       return
 
+    } catch (e: any) {
+      if (e.code === 'ER_DUP_ENTRY') {
+        res.status(400).json({
+          status: "error",
+          message: "Le numéro de colis existe déjà",
+        })
+      } else {
+        res.status(500).json({
+          status: "error",
+          message: "Internal server error",
+        })
+      }
+      return
+    }
+  }
+))
+
+
+router.delete("/package", expressAsyncHandler(
+  async (req, res) => {
+    console.log("request body", req.body)
+    const { articleReference, packageNumber, description } = req.body
+
+    if (!packageNumber) {
+      res.status(400).json({
+        status: "error",
+        message: "Missing required information",
+      })
+      return
+    }
+    
+    try {
+
+      await connection.promise().query(
+        "DELETE FROM package WHERE packageNumber = ?",
+        [packageNumber]
+      )
+  
+      res.status(200).json({
+        status: "success",
+        message: "Package deleted",
+      })
+      return
+
     } catch (e) {
       res.status(500).json({
         status: "error",
-        message: "Internal server error",
+        message: "Package not found",
       })
       return
     }
